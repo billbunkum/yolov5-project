8f83e2acaadd0a84ac656d8505f508a4d9e5db1e
billbunkum <20425559+billbunkum@users.noreply.github.com>
2024-03-12 19:54:10 -0400

    test if yaml dataset works

--- detect.py
+++ detect.py
@@ -4,6 +4,7 @@ Run YOLOv5 detection inference on images, videos, directories, globs, YouTube, w
 
 Usage - sources:
     $ python detect.py --weights yolov5s.pt --source 0                               # webcam
+    $ python detect.py --source ./inference/images/ --weights yolov5s.pt --conf 0.4
                                                      img.jpg                         # image
                                                      vid.mp4                         # video
                                                      screen                          # screenshot
@@ -69,12 +70,12 @@ from utils.torch_utils import select_device, smart_inference_mode
 @smart_inference_mode()
 def run(
     weights=ROOT / "yolov5s.pt",  # model path or triton URL
-    source=ROOT / "data/images",  # file/dir/URL/glob/screen/0(webcam)
-    #source=ROOT / "",
-    #data=ROOT / "data/coco128.yaml",  # dataset.yaml path
-    data=ROOT / "data/*.yaml",
+    #source=ROOT / "data/images/train2017/",  # file/dir/URL/glob/screen/0(webcam)
+    source=ROOT / "inference/images-uk",
+    #data=ROOT / "datasets/coco128.yaml",  # dataset.yaml path
+    data=ROOT / "data/data.yaml",
     imgsz=(640, 640),  # inference size (height, width)
-    conf_thres=0.25,  # confidence threshold
+    conf_thres=0.4,  # confidence threshold
     iou_thres=0.45,  # NMS IOU threshold
     max_det=1000,  # maximum detections per image
     device="",  # cuda device, i.e. 0 or 0,1,2,3 or cpu
